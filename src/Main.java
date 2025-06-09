4bee944651d6e20ce6f60d00a01a08db2a6c5211
PC <nikitaetvesh@yandex.ru>
2025-06-09 20:25:03 +0300

    Homework variables Lesson 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,70 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Homework variables Lesson 2");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        public class variablesTask1{
+            public static void main(String[] args) {
+                int a = 10000;
+                byte b = 30;
+                short c = 330;
+                long d = 5220330;
+                float e = 15.7f;
+                double f = 308.1;
+                System.out.println("Значение переменной с типом int равно " + a );
+                System.out.println("Значение переменной с типом byte равно " + b);
+                System.out.println("Значение переменной с типом short равно " + c);
+                System.out.println("Значение переменной с типом long равно " + d);
+                System.out.println("Значение переменной с типом float равно " + e);
+                System.out.println("Значение переменной с типом double равно " + f);
+            }
         }
+
+        System.out.println();
+
+        float g = 27.12f;
+        long h = 987678965549L;
+        double j = 2.786;
+        short k = 569;
+        short l = -159;
+        short i = 27897;
+        byte o = 67;
+
+        byte studentsLyudmilaPavlovna = 23;
+        byte studentsAnnaSergeevna = 27;
+        byte studentsEkaterinaAndreevna = 30;
+        short sheetsPaper = 480;
+        int sheetsForEach = sheetsPaper / (studentsLyudmilaPavlovna + studentsAnnaSergeevna + studentsEkaterinaAndreevna);
+        System.out.println("На каждого ученика рассчитано " + sheetsForEach + " листов бумаги");
+
+        System.out.println();
+
+        byte bottleMakingMachinePerfomanceIn2Minutes = 16;
+        byte bottleMakingMachinePerfomanceIn1Minute = 16 / 2;
+        int bottleMakingMachinePerfomanceIn20Minutes = bottleMakingMachinePerfomanceIn1Minute * 20;
+        int bottleMakingMachinePerfomancePerDay = bottleMakingMachinePerfomanceIn1Minute * (60*24);
+        int bottleMakingMachinePerfomanceIn3Day = bottleMakingMachinePerfomanceIn1Minute * ((60*24)*3);
+        int bottleMakingMachinePerfomanceFor1month = bottleMakingMachinePerfomanceIn1Minute * ((60*24)*30);
+        System.out.println("За 2 минуты машина произвела " + bottleMakingMachinePerfomanceIn2Minutes + " штук бутылок");
+        System.out.println("За 1 минуту машина произвела " + bottleMakingMachinePerfomanceIn1Minute + "штук бутылок");
+        System.out.println("За 20 минут машина произвела " + bottleMakingMachinePerfomanceIn20Minutes + " штук бутылок");
+        System.out.println("За сутки машина произвела " + bottleMakingMachinePerfomancePerDay + " штук бутылок");
+        System.out.println("За 3 дня машина произвела " + bottleMakingMachinePerfomanceIn3Day + " штук бутылок");
+        System.out.println("За 1 месяц машина произвела " + bottleMakingMachinePerfomanceFor1month + " штук бутылок");
+
+        System.out.println();
+
+        short theRequiredNumberOfCansOfPaintForSchool = 120;
+        byte whitePaintIsOneClass = 2;
+        byte brownPaintIsOneClass = 4;
+        int totalPaintConsumptionPerClass = (whitePaintIsOneClass + brownPaintIsOneClass);
+        int totalClasses = theRequiredNumberOfCansOfPaintForSchool / totalPaintConsumptionPerClass;
+        int totalCansOfWhitePaint = totalClasses * whitePaintIsOneClass;
+        int totalCansOfBrownPaint = totalClasses * brownPaintIsOneClass;
+        System.out.println("В школе, где " + totalClasses + " классов, нужно " + totalCansOfWhitePaint + " банок белой краски и " + totalCansOfBrownPaint + " банок коричневой краски");
+
+        System.out.println();
+
+        byte bananas = 5;
+        byte
     }
-}
+    }
